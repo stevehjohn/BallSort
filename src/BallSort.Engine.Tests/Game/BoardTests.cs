a6e23295f52e804bb56cc1a3e4986866c13c5acb
Stevo John <stevehuwjohn@gmail.com>
2025-09-08 13:04:01 +0100

    Adding tests

--- src/BallSort.Engine.Tests/Game/BoardTests.cs
+++ src/BallSort.Engine.Tests/Game/BoardTests.cs
@@ -1,3 +1,4 @@
+using BallSort.Engine.Game;
 using Xunit;
 
 namespace BallSort.Engine.Tests.Game;
@@ -6,8 +7,17 @@ public class BoardTests
 {
     [Theory]
     [InlineData("1,1,1,1,1,1,0,0,0,0,0,0", 4, 0, "")]
-    public void MoveThrowsForInvalidMoves(string layout, int source, int target, string exception)
+    public void MoveThrowsForInvalidMoves(string layout, int source, int target, string expectedMessage)
     {
-        
+        var board = new Board();
+
+        try
+        {
+            board.Move(source, target);
+        }
+        catch (Exception exception)
+        {
+            Assert.Equal(expectedMessage, exception.Message);
+        }
     }
 }
