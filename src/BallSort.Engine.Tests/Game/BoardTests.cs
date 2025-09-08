503fc476fc885dd3f11c3c38384fbf7f962a50d4
Stevo John <stevehuwjohn@gmail.com>
2025-09-08 13:02:35 +0100

    Code refactor

--- src/BallSort.Engine.Tests/Game/BoardTests.cs
+++ src/BallSort.Engine.Tests/Game/BoardTests.cs
@@ -1,6 +1,13 @@
+using Xunit;
+
 namespace BallSort.Engine.Tests.Game;
 
 public class BoardTests
 {
-    
+    [Theory]
+    [InlineData("1,1,1,1,1,1,0,0,0,0,0,0", 4, 0, "")]
+    public void MoveThrowsForInvalidMoves(string layout, int source, int target, string exception)
+    {
+        
+    }
 }
