d601f139c945e0f0490dafebe84b5a0bd35d9a58
Stevo John <stevehuwjohn@gmail.com>
2025-09-08 14:24:28 +0100

    Adding tests

---
+++ src/BallSort.Engine.Tests/Infrastructure/PuzzleManagerTests.cs
@@ -0,0 +1,28 @@
+using BallSort.Engine.Infrastructure;
+using Xunit;
+
+namespace BallSort.Engine.Tests.Infrastructure;
+
+[CollectionDefinition("NonParallel", DisableParallelization = true)]
+public class PuzzleManagerTests
+{
+    [Fact]
+    public void GetPuzzleReturnsExpectedBoard()
+    {
+        PuzzleManager.Path = "Test Data/SmallPuzzle.json";
+        
+        var board = PuzzleManager.Instance.GetPuzzle(0);
+        
+        Assert.Equal(4, board.Width);
+        
+        Assert.Equal(3, board.Height);
+        
+        Assert.Equal("", string.Join(',', board.GetColumn(0)));
+        
+        Assert.Equal("", string.Join(',', board.GetColumn(1)));
+        
+        Assert.Equal("Empty,Empty,Empty", string.Join(',', board.GetColumn(2)));
+        
+        Assert.Equal("Empty,Empty,Empty", string.Join(',', board.GetColumn(3)));
+    }
+}
