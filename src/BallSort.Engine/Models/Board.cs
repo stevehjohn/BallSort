bf95f24a88cefd4c4d7f0745a4be8186fe97a8d2
Stevo John <stevehuwjohn@gmail.com>
2025-09-08 11:40:46 +0100

    Adding engine

--- src/BallSort.Engine/Models/Board.cs
+++ src/BallSort.Engine/Models/Board.cs
@@ -2,11 +2,25 @@ namespace BallSort.Engine.Models;
 
 public class Board
 {
-    
+    private int[][] _columns;
     
     public Board(Puzzle puzzle)
     {
+        _columns = new int[puzzle.GridWidth][];
+
+        var index = 0;
         
+        for (var column = 0; column < puzzle.GridWidth; column++)
+        {
+            _columns[column] = new int[puzzle.GridHeight];
+
+            for (var row = 0; row < puzzle.GridHeight; row++)
+            {
+                _columns[column][row] = puzzle.Data.Layout[index];
+                
+                index++;
+            }
+        }
     }
     
     public Board Clone()
