c3ad5a348f85aae4a5598b0cde2cb862b3a961b0
Stevo John <stevehuwjohn@gmail.com>
2025-09-09 12:41:28 +0100

    Work in progress

--- src/BallSort.Engine.Tests/Game/BoardTests.cs
+++ src/BallSort.Engine.Tests/Game/BoardTests.cs
@@ -158,4 +158,12 @@ public class BoardTests
         
         Assert.Equal(expectedCapacity, board.Capacity(column));
     }
+
+    [Fact]
+    public void ColoursReturnsCorrectValue()
+    {
+        var board = "1,1,1,2,3,3,4,4,5,0,0,0".BoardFromLayout(4, 3);
+        
+        Assert.Equal(5, board.Colours);
+    }
 }
