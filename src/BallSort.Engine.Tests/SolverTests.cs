ac04b920e6ab6962a014a461eebac2d840a23e38
Stevo John <stevehuwjohn@gmail.com>
2025-09-09 11:47:18 +0100

    Work in progress

--- src/BallSort.Engine.Tests/SolverTests.cs
+++ src/BallSort.Engine.Tests/SolverTests.cs
@@ -1,6 +1,12 @@
+using Xunit;
+
 namespace BallSort.Engine.Tests;
 
 public class SolverTests
 {
-    
+    [Theory]
+    [InlineData("", "")]
+    public void SolveReturnsExpectedMoves(string layout, string moves)
+    {
+    }
 }
