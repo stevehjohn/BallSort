abb0bd989a4cc9e18f8ab5b07f3221893b3c1265
Stevo John <stevehuwjohn@gmail.com>
2025-09-12 18:25:42 +0100

    Removed test

--- src/BallSort.Engine.Tests/SolverTests.cs
+++ src/BallSort.Engine.Tests/SolverTests.cs
@@ -6,34 +6,4 @@ namespace BallSort.Engine.Tests;
 
 public class SolverTests
 {
-    [Theory]
-    [InlineData("1,2,2,1,0,0,0,0", 4, 2, "0,2|1,3|0,3|1,2")]
-    public void SolveReturnsExpectedMoves(string layout, int width, int height, string moves)
-    {
-        var solver = new Solver(layout.BoardFromLayout(width, height));
-
-        var solution = solver.Solve();
-
-        var expectedMoves = new List<Move>();
-
-        var movesSplit = moves.Split('|', StringSplitOptions.TrimEntries | StringSplitOptions.RemoveEmptyEntries);
-
-        foreach (var move in movesSplit)
-        {
-            var moveSplit = move.Split(',', StringSplitOptions.TrimEntries | StringSplitOptions.RemoveEmptyEntries);
-            
-            expectedMoves.Add(new Move(int.Parse(moveSplit[0]), int.Parse(moveSplit[1])));
-        }
-        
-        Assert.Equal(expectedMoves.Count, solution.Count);
-
-        var i = 0;
-        
-        foreach (var expectedMove in expectedMoves)
-        {
-            Assert.Equal(expectedMove, solution[i]);
-            
-            i++;
-        }
-    }
 }
