a033d5c5d59dca194e0968f0679529da71586697
Stevo John <stevehuwjohn@gmail.com>
2025-09-13 12:12:36 +0100

    Stuff

--- src/BallSort.Engine.Tests/Infrastructure/PuzzleManagerTests.cs
+++ src/BallSort.Engine.Tests/Infrastructure/PuzzleManagerTests.cs
@@ -17,17 +17,17 @@ public class PuzzleManagerTests
         
         Assert.Equal(4, board.Height);
         
-        Assert.Equal("DarkPurple,Red,Yellow,Green", string.Join(',', board.GetColumn(0)));
+        Assert.Equal("Yellow,Green,Orange,Yellow", string.Join(',', board.GetColumn(0)));
         
-        Assert.Equal("Yellow,Green,Red,Orange", string.Join(',', board.GetColumn(1)));
+        Assert.Equal("Green,Red,Yellow,Orange", string.Join(',', board.GetColumn(1)));
         
-        Assert.Equal("Blue,Blue,Blue,Red", string.Join(',', board.GetColumn(2)));
+        Assert.Equal("Blue,Green,DarkPurple,Blue", string.Join(',', board.GetColumn(2)));
         
-        Assert.Equal("DarkPurple,Orange,Orange,Green", string.Join(',', board.GetColumn(3)));
+        Assert.Equal("DarkPurple,Blue,Red,Red", string.Join(',', board.GetColumn(3)));
         
-        Assert.Equal("DarkPurple,Yellow,Green,Blue", string.Join(',', board.GetColumn(4)));
+        Assert.Equal("Orange,Red,Yellow,DarkPurple", string.Join(',', board.GetColumn(4)));
         
-        Assert.Equal("DarkPurple,Red,Yellow,Orange", string.Join(',', board.GetColumn(5)));
+        Assert.Equal("DarkPurple,Orange,Blue,Green", string.Join(',', board.GetColumn(5)));
         
         Assert.Equal("Empty,Empty,Empty,Empty", string.Join(',', board.GetColumn(6)));
         
