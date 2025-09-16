eaa44f2125465e60b059b020bd10385c15494b7a
Stevo John <stevehuwjohn@gmail.com>
2025-09-16 12:59:24 +0100

    WIP

--- src/BallSort.Engine.Tests/SolverTests.cs
+++ src/BallSort.Engine.Tests/SolverTests.cs
@@ -1,3 +1,5 @@
+using System.Text;
+using BallSort.Engine.Game;
 using BallSort.Engine.Infrastructure;
 using Xunit;
 
@@ -13,12 +15,42 @@ public class SolverTests
         for (var i = 0; i < puzzleManager.PuzzleCount; i++)
         {
             var board = puzzleManager.GetPuzzle(i);
+            
+            DumpBoard(board);
 
             var solver = new Solver(board);
 
             var result = solver.Solve();
 
             Assert.True(result.Solved);
+            
+            DumpBoard(board);
+        }
+    }
+
+    private void DumpBoard(Board board)
+    {
+        Console.WriteLine();
+        
+        var builder = new StringBuilder();
+
+        var columns = new List<Colour[]>();
+        
+        for (var x = 0; x < board.Width; x++)
+        {
+            columns.Add(board.GetColumn(x));
+        }
+
+        for (var y = 0; y < board.Height; y++)
+        {
+            for (var x = 0; x < board.Width; x++)
+            {
+                builder.Append((char) ('@' + (int) columns[x][y]));
+            }
+
+            Console.WriteLine($"  {builder}");
+
+            builder.Clear();
         }
     }
 }
