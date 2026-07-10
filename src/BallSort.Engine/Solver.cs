2a78b57ebc5abdcc8931b00afecc5bf73687ab7c
Stevo John <4673357+stevehjohn@users.noreply.github.com>
2026-07-10 16:50:14 +0100

    Bound shorter solution search

--- src/BallSort.Engine/Solver.cs
+++ src/BallSort.Engine/Solver.cs
@@ -6,6 +6,8 @@ namespace BallSort.Engine;
 
 public class Solver
 {
+    private const int MaxImprovementNodes = 250_000;
+
     private readonly Board _board;
 
     private readonly MoveGenerator _moveGenerator;
@@ -16,7 +18,11 @@ public class Solver
 
     private readonly HashSet<ulong[]> _visited = new(new BoardHashEqualityComparer());
 
+    private readonly Dictionary<ulong[], int> _bestDepthByHash = new(new BoardHashEqualityComparer());
+
     private List<Move> _bestMoves;
+
+    private int _improvementNodes;
 
     public Solver(Board board)
     {
@@ -33,20 +39,77 @@ public class Solver
 
         _visited.Clear();
 
+        _bestDepthByHash.Clear();
+
         _bestMoves = null;
+
+        _improvementNodes = 0;
 
         _visited.Add(_boardHasher.GetHash());
 
-        Explore();
+        if (!ExploreFirstSolution())
+        {
+            return (false, null);
+        }
 
-        return _bestMoves == null
-            ? (false, null)
-            : (true, _bestMoves);
+        _moves.Clear();
+
+        _bestDepthByHash.Clear();
+
+        _bestDepthByHash.Add(_boardHasher.GetHash(), 0);
+
+        ExploreShorterSolutions();
+
+        return (true, _bestMoves);
     }
 
-    private void Explore()
+    private bool ExploreFirstSolution()
     {
-        if (_bestMoves != null && _moves.Count >= _bestMoves.Count)
+        var lastMove = _moves.Count > 0 ? _moves.Peek() : Move.NullMove;
+
+        var moves = _moveGenerator.GetMoves(lastMove);
+
+        foreach (var move in moves)
+        {
+            _board.Move(move);
+
+            if (_board.IsSolved())
+            {
+                _moves.Push(move);
+
+                SaveBestSolution();
+
+                return true;
+            }
+
+            var hash = _boardHasher.GetHash();
+
+            if (_visited.Add(hash))
+            {
+                _moves.Push(move);
+
+                if (ExploreFirstSolution())
+                {
+                    return true;
+                }
+
+                _moves.Pop();
+            }
+
+            _board.UndoLastMove();
+        }
+
+        return false;
+    }
+
+    private void ExploreShorterSolutions()
+    {
+        if (_improvementNodes++ >= MaxImprovementNodes)
+        {
+            return;
+        }
+
+        if (_moves.Count >= _bestMoves.Count - 1)
         {
             return;
         }
@@ -69,11 +132,9 @@ public class Solver
             {
                 var hash = _boardHasher.GetHash();
 
-                if (_visited.Add(hash))
+                if (ShouldExplore(hash, _moves.Count))
                 {
-                    Explore();
-
-                    _visited.Remove(hash);
+                    ExploreShorterSolutions();
                 }
             }
 
@@ -81,6 +142,18 @@ public class Solver
 
             _board.UndoLastMove();
         }
+    }
+
+    private bool ShouldExplore(ulong[] hash, int depth)
+    {
+        if (_bestDepthByHash.TryGetValue(hash, out var bestDepth) && bestDepth <= depth)
+        {
+            return false;
+        }
+
+        _bestDepthByHash[hash] = depth;
+
+        return true;
     }
 
     private void SaveBestSolution()
